7518b3c087dba5f9eedd54337eabdfd27a0f85d3
Scott Chamberlain <myrmecocystus@gmail.com>
2014-11-11 08:14:32 -0800

    first commit

---
+++ R/code.R
@@ -0,0 +1,50 @@
+#' Get metadata for an object id.
+#'
+#' @importFrom httr GET content stop_for_status
+#' @importFrom jsonlite fromJSON
+#' @importFrom XML xpathApply xpathSApply xmlValue htmlParse
+#' @export
+#'
+#' @param id An object id
+#' @param ... Curl args passed on to \code{\link[httr]{GET}}
+#' @examples \donttest{
+#' muse_get(559490)
+#' muse_get(559490)$name
+#' muse_get(559490)$values
+#' muse_get(246562)
+#' }
+muse_get <- function(id, ...){
+  out <- musemeta_GET(paste0(mmbase(), id), ...)
+  muse_parse(out)
+}
+
+#' @export
+print.muse <- function(x, ...){
+  cat(sprintf("<Museum metadata> %s", x$name), sep = "\n")
+  for(i in seq_along(x$values)){
+    cat(sprintf("  %s: %s", x$values[[i]]$name, x$values[[i]]$value), sep = "\n")
+  }
+}
+
+musemeta_GET <- function(url, args = list(), ...){
+  res <- GET(url, query = args, ...)
+  stop_for_status(res)
+  content(res, "text")
+}
+
+muse_parse <- function(x){
+  tmp <- htmlParse(x)
+  tcon <- xpathApply(tmp, "//div[@class='tombstone-container']")[[1]]
+  name <- xmlValue(xpathApply(tcon, "h2")[[1]])
+  tomb <- xpathApply(tcon, "//div[@class='tombstone']")[[1]]
+  tags <- lapply(xpathApply(tomb, "div"), function(x){
+    list(name = gsub(":", "", xpathSApply(x, "strong", xmlValue)),
+         value = gsub("^\\s+", "", gsub(".+\n|.+\r", "", xmlValue(x))))
+  })
+  structure(list(name=name, values=tags), class="muse")
+}
+
+mmbase <- function() "http://www.metmuseum.org/collection/the-collection-online/search/"
+
+# url <- 'http://www.metmuseum.org/collection/the-collection-online/search/559490?rpp=30&pg=1&rndkey=20140815&ft=*&what=Bronze&pos=1'
+# url <- 'http://www.metmuseum.org/collection/the-collection-online/search/559490'
---
+++ R/musemeta.R
@@ -0,0 +1,9 @@
+#' R client for museum metadata
+#'
+#' @name musemeta-package
+#' @aliases musemeta
+#' @docType package
+#' @title R client for museum metadata
+#' @author Scott Chamberlain \email{myrmecocystus@@gmail.com}
+#' @keywords package
+NULL
