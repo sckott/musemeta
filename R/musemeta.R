b66b00309ddeeacf58062e9cd85fbdd7214a7f57
Scott Chamberlain <myrmecocystus@gmail.com>
2020-07-23 17:56:55 -0700

    remove unused pkgs

--- R/musemeta.R
+++ R/musemeta.R
@@ -2,7 +2,6 @@
 #' @description R client for museum metadata
 #' @importFrom crul HttpClient
 #' @importFrom jsonlite fromJSON
-#' @importFrom httr GET content stop_for_status config
 #' @importFrom xml2 read_html xml_find_first xml_text xml_find_all xml_attr
 #' xml_children xml_name
 #' @importFrom ckanr changes package_list group_list group_show tag_list tag_show
